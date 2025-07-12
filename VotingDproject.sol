af4fdb88ffd8396a77944dd7e97cb36dbac21732
SaqibMalik822 <saqibmalik5374@gmail.com>
2025-07-12 20:40:39 +0530

    Initial commit

---
+++ VotingDproject.sol
@@ -0,0 +1,21 @@
+// SPDX-Licens-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+contract ApplicationSubmission{
+    struct Candidate{
+        string memory name;
+        string memory Education;
+        string memory Village;
+        uint8 Age;
+    }
+
+    function application(string memory _name,
+    string memory _education, string memory _village, uint8 _age){
+        
+    }
+}
+
+contract Voting{
+
+}
