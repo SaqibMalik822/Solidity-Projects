085056409483ec5e779906b6a7b966b57424ad6d
SaqibMalik822 <saqibmalik5374@gmail.com>
2025-07-13 15:29:29 +0530

    initial-commit

---
+++ funds.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+// get funds from user
+// withdraw funds
+// set a minimum funding value of 5 dollars
+
+contract FundMe{
+
+    function fund() public payable{
+
+        require(msg.value > 1e18, "you did not send enough eth");
+
+    }
+
+
+    // function withdraw() public{}
+
+
+
+
+}
